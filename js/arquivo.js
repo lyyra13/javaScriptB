f31d7594ec37bea83b24cb39bc7aef6d0a9f8b6c
lyyra13 <ysantos.slarissa@gmail.com>
2025-07-01 12:25:22 -0300

    Aula de javaScript 01/07

---
+++ js/arquivo.js
@@ -0,0 +1,24 @@
+var global = "Variavel global";
+console.log(global);
+
+function exemplo2(){
+    var local  = "Variavel local";
+    console.log(local);
+}
+exemplo2();
+
+let x = 10;
+
+if(x > 5){
+    let y = 20
+    console.log("Valor de y: ", y)
+}
+console.log(x);
+//console.log(y);
+
+const _constante = "Texto";
+console.log(_constante)
+
+function saudacao(){
+    alert("Bem vindo ao nosso site3")
+}
